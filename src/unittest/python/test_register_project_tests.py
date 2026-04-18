7717ca2a313fb91e5f4aea563542fc325d742576
Linus Munn <lmunn@umass.edu>
2026-04-18 15:35:13 +0200

    Wrote first invalid test (TC4) and developed idea for test structure

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -4,9 +4,13 @@ from uc3m_consulting import EnterpriseManager, EnterpriseManagementException
 
 class TestRegisterProject(unittest.TestCase):
     """class for testing the register_order method"""
-    def test_something( self ):
-        """dummy test"""
-        self.assertEqual(True, False)
+    def test_TC4( self ):
+        manager = EnterpriseManager()
+
+        with self.assertRaises(EnterpriseManagementException):
+            manager.register_document(
+                "desktop/register_document/deletion/tc5-invalid_pdf.json"
+            )
 
 
 if __name__ == '__main__':
